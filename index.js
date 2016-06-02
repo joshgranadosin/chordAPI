4315766048c000df691ba54e9111ba8f91fe9f7e
Joshua Granadosin <joshgranadosin@Christabelles-MacBook-Pro.local>
2016-06-01 22:09:45 -0700

    Added better error handling

--- index.js
+++ index.js
@@ -14,18 +14,27 @@ var Pianochord = require('./models/pianochord.js');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
+var badrequest = {'status':400, 'err':'default',
+		'message':"requests must be made in the following format: " +
+		"https://chords-api-app.herokuapp.com/:type/:note/:modifier where " +
+		":type is either 'piano' or 'guitar', " +
+		":note is the note letter (A-G) with a flat(Ab) or a sharp(B%23), " +
+		":modifier is either 'major' or 'minor'"}
+
 app.get('/piano/:note', function(req,res){
 	console.log('get#piano#default');
 	var note = req.params.note;
 	var mod = 'major';
 
 	Pianochord.findOne({type:mod}, function(err, data){
-		if(!err){
+		if(!err && data != null){
 			console.log(data);
 			res.json(data.data[note]);
 		}
 		else{
 			console.log(err);
+			badrequest.err = err;
+			res.status(400).json(badrequest);
 		}
 	});
 });
@@ -36,12 +45,14 @@ app.get('/guitar/:note', function(req,res){
 	var mod = 'major'
 
 	Guitarchord.findOne({type:mod}, function(err, data){
-		if(!err){
+		if(!err && data != null){
 			console.log(data);
 			res.json(data.data[note]);
 		}
 		else{
 			console.log(err);
+			badrequest.err = err;
+			res.status(400).json(badrequest);
 		}
 	});
 });
@@ -52,12 +63,14 @@ app.get('/piano/:note/:mod', function(req,res){
 	var mod = req.params.mod;
 
 	Pianochord.findOne({type:mod}, function(err, data){
-		if(!err){
+		if(!err && data != null){
 			console.log(data);
 			res.json(data.data[note]);
 		}
 		else{
 			console.log(err);
+			badrequest.err = err;
+			res.status(400).json(badrequest);
 		}
 	});
 });
@@ -71,22 +84,21 @@ app.get('/guitar/:note/:mod', function(req,res){
 	console.log(mod);
 
 	Guitarchord.findOne({type:mod}, function(err, data){
-		if(!err){
+		if(!err && data != null){
 			console.log(data);
 			res.json(data.data[note]);
 		}
 		else{
 			console.log(err);
+			badrequest.err = err;
+			res.status(400).json(badrequest);
 		}
 	});
 });
 
 app.get('/*', function(req,res){
-	res.status(400).json({'status':400,'message':"requests must be made in the following format: " +
-		"https://chords-api-app.herokuapp.com/:type/:note/:modifier where " +
-		":type is either 'piano' or 'guitar', " +
-		":note is the note letter (A-G) with a flat(Ab) or a sharp(B%23), " +
-		":modifier is either 'major' or 'minor'"});
+	res.status(400).json(badrequest);
 });
 
 app.listen(process.env.PORT || 3000);
+
